fb9fbf16eb10d8812398ec131400dbc344a54535
nreese <reese.nathan@gmail.com>
2016-08-16 18:51:10 -0600

    update bootstrap-addons dependency

--- public/lib/bootstrap-addons/dist/js/bootstrap-addons.js
+++ public/lib/bootstrap-addons/dist/js/bootstrap-addons.js
@@ -2,7 +2,9 @@ angular.module('BootstrapAddons', ['BootstrapAddonsTemplateCache']);
 angular.module('BootstrapAddons')
 .directive('timeslider', ["$document", "$timeout", function($document, $timeout) {
   const SVG_ID = "timesliderSvg";
+  const MIN_LABEL_SPACING = 60;
   const CONTROLS_WIDTH = 72; //fixed width of buttons used to advance timeslider
+  const MS_PER_DAY = 86400000;
   return {
     restrict: 'E',
     scope: {
@@ -10,7 +12,6 @@ angular.module('BootstrapAddons')
       onClear: "&",
       onChange: "&",
       start: "=",
-      ticks: "@",
       width: "@",
     },
     templateUrl: 'timeslider/timeslider.html',
@@ -24,13 +25,6 @@ angular.module('BootstrapAddons')
       scope.actionIcon = "glyphicon-play";
       var intervalId = null;
       drawTimeline();
-
-      scope.$watch('end', function(newVal, oldVal) {
-        if(newVal.getTime() !== oldVal.getTime()) {
-          scope.end = newVal;
-          redraw();
-        }
-      }, true);
 
       scope.$on('timesliderForceRender', function() {
         redraw();
@@ -80,9 +74,9 @@ angular.module('BootstrapAddons')
         //update extent
         brush.extent([start, end]);
         //draw brush to match new extent
-        brush(d3.select('.brush'));
+        brush(d3.select('.ba-brush'));
         //fire brush events
-        brush.event(d3.select('.brush'));
+        brush.event(d3.select('.ba-brush'));
       }
 
       function pause() {
@@ -116,7 +110,7 @@ angular.module('BootstrapAddons')
         drawTimeline();
         if(oldBrush[0].getTime() !== oldBrush[1].getTime()) {
           brush.extent([oldBrush[0], oldBrush[1]]);
-          brush(d3.select('.brush'));
+          brush(d3.select('.ba-brush'));
         }
       }
 
@@ -125,8 +119,10 @@ angular.module('BootstrapAddons')
           scope.width = elem[0].clientWidth - CONTROLS_WIDTH;
         }
         if(scope.width <= 0) scope.width = CONTROLS_WIDTH * 2;
+        var numTicks = scope.width / MIN_LABEL_SPACING;
+        var timeWidth = scope.end.getTime() - scope.start.getTime();
 
-        var container = d3.select("#timesliderContainer");
+        var container = d3.select("#ba-timeslider-container");
         container.selectAll('#' + SVG_ID).remove();
         var svg = container.append('svg')
           .attr("id", SVG_ID)
@@ -137,43 +133,50 @@ angular.module('BootstrapAddons')
           .attr("y", 0)
           .attr("width", scope.width)
           .attr("height", 22)
-          .attr("class", 'timeslider');
+          .attr("class", 'ba-timeslider');
 
         var timeScale = d3.time.scale.utc()
           .domain([scope.start.getTime(), scope.end.getTime()])
           .range([0, parseInt(scope.width, 10)]);
+        var endPixelLocation = timeScale(scope.end.getTime());
 
         //draw time scale ticks
-        timeScale.ticks(parseInt(scope.ticks, 10)).forEach(function(tick, index) {
+        var lastTick = null;
+        timeScale.ticks(numTicks).forEach(function(tick, index) {
+          var tickPixelLocation = timeScale(tick);
           svg.append('line')
-            .attr('class', 'scaleLine')
-            .attr('x1', timeScale(tick))
-            .attr('x2', timeScale(tick))
+            .attr('class', 'ba-scaleline')
+            .attr('x1', tickPixelLocation)
+            .attr('x2', tickPixelLocation)
             .attr('y1', 0)
             .attr('y2', 22);
 
-          if(index % 2 != 0) {
-            var hours = tick.getUTCHours();
-            if (hours < 10) hours = "0" + hours;
+          //Only add label to line if not close to start or end
+          if(tickPixelLocation > 25 && (endPixelLocation - tickPixelLocation) > 25) {
+            var format = getTimeFormator(timeWidth, tick, lastTick);
+            var label = format(tick).split(" ");
+            if(label.length === 1) label.push("");
+            lastTick = tick;
             svg.append('text')
-              .attr('class', 'scaleLabel')
-              .attr('x', timeScale(tick))
-              .attr('dx', '-1.25em')
+              .attr('class', 'ba-scalelabel')
+              .attr('x', tickPixelLocation)
+              .attr('dx', function() {
+                var dx = '-1.25em';
+                if(label[0].length > 2) var dx = '-2.25em';
+                return dx;
+              })
               .attr('y', 11)
               .attr('text-anchor', 'left')
               .attr('dominant-baseline', 'middle')
-              .text(hours);
-
-            var mins = tick.getUTCMinutes();
-            if (mins < 10) mins = "0" + mins;
+              .text(label[0]);
             svg.append('text')
-              .attr('class', 'scaleLabel')
-              .attr('x', timeScale(tick))
+              .attr('class', 'ba-scalelabel')
+              .attr('x', tickPixelLocation)
               .attr('dx', '0.25em')
               .attr('y', 11)
               .attr('text-anchor', 'right')
               .attr('dominant-baseline', 'middle')
-              .text(mins);
+              .text(label[1]);
           }
         });
 
@@ -181,12 +184,39 @@ angular.module('BootstrapAddons')
           .x(timeScale)
           .on('brushend', brushend);
         svg.append('g')
-          .attr('class', 'brush')
+          .attr('class', 'ba-brush')
           .call(brush)
           .selectAll('rect')
             .attr('height', 22);
       }
+
+      //https://github.com/d3/d3-3.x-api-reference/blob/master/Time-Formatting.md
+      function getTimeFormator(timeWidth, tick, lastTick) {
+        var format = null;
+        if (timeWidth <= MS_PER_DAY) {
+          if(!lastTick) {
+            format = d3.time.format.utc('%a %d');
+          } else {
+            format = d3.time.format.utc('%H %M');
+          }
+        } else if (timeWidth <= (MS_PER_DAY * 28)) {
+          if(lastTick && tick.getUTCDate() === lastTick.getUTCDate()) {
+            format = d3.time.format.utc('%H %M');
+          } else {
+            format = d3.time.format.utc('%a %d');
+          }
+        } else if (timeWidth <= (MS_PER_DAY * 365)) {
+          format = d3.time.format.utc('%b %d');
+        } else {
+          if(!lastTick || tick.getUTCFullYear() !== lastTick.getUTCFullYear()) {
+            format = d3.time.format.utc('%b %Y');
+          } else {
+            format = d3.time.format.utc('%b %d');
+          }
+        }
+        return format;
+      }
     }
   }
 }]);
-angular.module("BootstrapAddonsTemplateCache", []).run(["$templateCache", function($templateCache) {$templateCache.put("timeslider/timeslider.html","<div><div class=\"btn-group\"><button class=\"btn btn-xs btn-primary\" ng-click=\"togglePlay()\"><span class=\"glyphicon {{actionIcon}}\"></span></button> <button class=\"btn btn-xs btn-primary\" ng-click=\"stepBack()\"><span class=\"glyphicon glyphicon-step-backward\"></span></button> <button class=\"btn btn-xs btn-primary\" ng-click=\"stepForward()\"><span class=\"glyphicon glyphicon-step-forward\"></span></button> <span id=\"timesliderContainer\"></span></div></div>");}]);
+angular.module("BootstrapAddonsTemplateCache", []).run(["$templateCache", function($templateCache) {$templateCache.put("timeslider/timeslider.html","<div><div class=\"btn-group\"><button class=\"btn btn-xs btn-primary\" ng-click=\"togglePlay()\"><span class=\"glyphicon {{actionIcon}}\"></span></button> <button class=\"btn btn-xs btn-primary\" ng-click=\"stepBack()\"><span class=\"glyphicon glyphicon-step-backward\"></span></button> <button class=\"btn btn-xs btn-primary\" ng-click=\"stepForward()\"><span class=\"glyphicon glyphicon-step-forward\"></span></button> <span id=\"ba-timeslider-container\"></span></div></div>");}]);
